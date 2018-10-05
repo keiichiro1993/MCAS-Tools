97cf6da22146db4428c246aab6a868f9319abf6a
kurosuke_coins <keiichiro1993@gmail.com>
2018-10-05 12:44:08 +0900

    first commit

---
+++ ToFTP.ps1
@@ -0,0 +1,34 @@
+﻿#Set TMG log location
+$TMGLogLocation = "C:\TMGLogs"
+
+#Set the log location of this script
+$date = Get-Date -Format FileDateTimeUniversal
+$TranscriptPath = "C:\TransferLogs\" + $date + ".log"
+Start-Transcript -Path $TranscriptPath -Append
+
+Import-Module PSFTP 
+
+$user = "" #FTP user name provided in MCAS portal 
+$pass = "" #FTP password provided in MCAS portal
+
+$str = ConvertTo-SecureString $pass -AsPlainText -Force 
+$psc = New-Object System.Management.Automation.PsCredential($user, $str) 
+$credential = Get-Credential -Credential $psc
+
+
+"/////////////////// Started Transfering ///////////////////"
+Get-Date
+
+"----------Set-FTPConnection---------"
+Set-FTPConnection -Credentials $credential -Server ftp://192.168.6.21 -UsePassive -KeepAlive -Verbose
+
+$file = Get-ChildItem -Path $TMGLogLocation | Sort-Object -Descending | Select-Object -Index 1
+echo $file
+
+"----------Add-FTPItem---------"
+Add-FTPItem -Path "/TMG" -LocalPath $file.FullName -Overwrite -Verbose
+
+Get-Date
+"/////////////////// Finished Transfering ///////////////////"
+
+Stop-Transcript
